c9e2c605efd41abc46964591df01a25363d72e96
Fr4nk1in <fushen@mail.ustc.edu.cn>
2025-12-23 13:58:28 +0000

    feat: better logging format

--- src/sfutils/utils/log.py
+++ src/sfutils/utils/log.py
@@ -5,7 +5,8 @@ _LOGGING_SETUP: bool = False
 
 def setup_logging(level: int | str | None = None) -> None:
     logging.basicConfig(
-        format="%(asctime)s [%(levelname)5s] %(message)s",
+        format="%(asctime)s | %(levelname)7s | %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
         level=level or logging.INFO,
         force=True,
     )
